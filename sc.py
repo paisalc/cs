ad55cdc9a8e38c2bc0344293a33383906b2ab04f
paisalc <117634359+paisalc@users.noreply.github.com>
2022-11-16 12:54:56 +0530

    Update sc.py

--- sc.py
+++ sc.py
@@ -1,13 +1,9 @@
 
-if BMI <= 18.4:
-    print("You are underweight.")
-elif BMI <= 24.9:
-    print("You are healthy.")
-elif BMI <= 29.9:
-    print("You are over weight.")
-elif BMI <= 34.9:
-    print("You are severely over weight.")
-elif BMI <= 39.9:
-    print("You are obese.")
-else:
-    print("You are severely obese.")
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+
+sum = float(num1) + float(num2)
+
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
